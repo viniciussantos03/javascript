7b3459897e0bd6327cb5c439a9c1c3df09b76016
viniciussantos03 <viniciusferreirasantos982@gmail.com>
2026-07-07 19:29:22 -0300

    else e if aninhados

---
+++ aula12/ex013.js
@@ -0,0 +1,12 @@
+var agora = new Date()
+var hora = agora.getHours()
+console.log(`Agora são exatamente ${hora} horas.`)
+if (hora > -1 && hora < 6) {
+    console.log('Boa Madrugada')
+} else if(hora < 12) {
+    console.log('Bom Dia')
+} else if (hora < 18) {
+    console.log('Boa Tarde')
+} else {
+    console.log('Boa Noite')
+}
