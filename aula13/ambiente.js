2ba1ebcaa021c9aa1bd973fe738329affd66bd01
viniciussantos03 <viniciusferreirasantos982@gmail.com>
2026-07-15 22:17:27 -0300

    usando while e do

---
+++ aula13/ambiente.js
@@ -0,0 +1,29 @@
+c = 1
+do {
+    console.log(`Posição ${c}`)
+    c++
+} while (c <= 500)
+
+
+
+
+/*
+c = 1
+while (c <= 500) {
+    console.log(`Posição ${c}`)
+    c++
+}
+*/
+
+
+
+
+
+/*
+console.log('Olá, Tudo Bem?')
+console.log('Olá, Tudo Bem?')
+console.log('Olá, Tudo Bem?')
+console.log('Olá, Tudo Bem?')
+console.log('Olá, Tudo Bem?')
+console.log('Olá, Tudo Bem?')
+*/
