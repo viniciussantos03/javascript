2ff9a4f7a0bcfc5a4d4e723246da6ab5724704ad
viniciussantos03 <viniciusferreirasantos982@gmail.com>
2026-07-28 11:32:45 -0300

    bugs resolvidos

--- desafios/tema-c-e/script.js
+++ desafios/tema-c-e/script.js
@@ -1,17 +1,18 @@
 let estacao = document.body
 let temasalvo = localStorage.getItem('tema')
+let botaotema = document.getElementById('tema')
 if (temasalvo == 'escuro') {
         estacao.classList.add('escuro')
+        botaotema.innerHTML = '<i class="fa-solid fa-sun"></i>'
     }
 
 function tema() {
-    let tema = document.getElementById('tema')
     estacao.classList.toggle('escuro')
     if (estacao.classList.contains('escuro')) {
-        tema.innerHTML = '<i class="fa-solid fa-sun"></i>'
+        botaotema.innerHTML = '<i class="fa-solid fa-sun"></i>'
         localStorage.setItem('tema', 'escuro')
 } else {
-        tema.innerHTML = '<i class="fa-solid fa-moon"></i>'
+        botaotema.innerHTML = '<i class="fa-solid fa-moon"></i>'
         localStorage.setItem('tema', 'claro')
     }
 }
