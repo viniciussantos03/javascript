acc6b4596fec39e1805fd4a65991df10dd864ed9
viniciussantos03 <viniciusferreirasantos982@gmail.com>
2026-08-03 17:37:41 -0300

    resumo básico

---
+++ praticas/resumo-dados-if/script/script.js
@@ -0,0 +1,124 @@
+// number
+/*
+let num = 10
+let num2 = 20
+let nome = 'Vinicius'
+let nada = NaN
+
+console.log(num)
+console.log(typeof num)
+console.log(num2)
+console.log(typeof num2)
+console.log(nada)
+console.log(typeof nada)
+
+console.log(num + num2)
+console.log(num + 5)
+console.log(num + " " + nome) 
+*/
+
+
+//string
+/*
+let nome = 'Vínicius'
+let SobreNome = "Ferreira Santos"
+
+console.log(nome)
+console.log(typeof nome)
+console.log(SobreNome)
+console.log(typeof SobreNome)
+
+console.log('Meu nome é ' + nome + ' ' + SobreNome)
+
+console.log("Eu goste de 'pizza'")
+*/
+
+//boolean
+
+/*
+let verdadeiro = true
+let falso = false
+
+console.log(verdadeiro)
+console.log(typeof verdadeiro)
+console.log(falso)
+console.log(typeof falso)
+*/
+
+//undefined e null
+//hoisting
+
+/*
+console.log(nome)
+
+let nome
+let nulo = null
+
+console.log(nome)
+console.log(typeof nome)
+nome = 'Vínicius'
+console.log(nome)
+console.log(typeof nome)
+
+console.log(nulo)
+console.log(typeof nulo) 
+*/
+
+//object
+/*
+let pessoa = {
+    nome: 'Vínicius',
+    idade: 17,
+    profissão: 'programador',
+    trabalho: true,
+}
+
+console.log(pessoa)
+console.log(typeof pessoa)
+console.log(pessoa.nome)
+console.log(pessoa.idade)
+console.log(typeof pessoa.idade)
+console.log(pessoa.profissão)
+console.log(pessoa.trabalho)
+
+console.log('Meu nome é ' + pessoa.nome)
+
+pessoa.nome = 'Leandrinha'
+pessoa.saldo = 1000
+
+console.log('Meu nome é ' + pessoa.nome)
+console.log(pessoa)
+console.log(pessoa.saldo)
+*/
+
+//array
+/*
+let arr = [17, 'Vinicius', true, null, 'macarrão']
+let arr2 = [1, 2, 3, 4, 5, 6]
+
+console.log(arr)
+console.log(typeof arr)
+console.log('Meu nome é ' + arr[1])
+console.log(arr2)
+console.log(typeof arr2)
+console.log(arr2[4])
+
+arr2[5] = 10
+
+console.log(arr2)
+
+arr2[6] = -10
+
+console.log(arr2)
+*/
+
+//condição if
+/*
+let idade = 12
+let MaiorQue = 18
+if (idade >= MaiorQue) {
+    console.log('Você é maior de idade')
+} else {
+    console.log('Você é menor de idade')
+}
+*/
