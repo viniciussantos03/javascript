d4cc6fa99b3098737a616d201fa8e59421bf181f
viniciussantos03 <viniciusferreirasantos982@gmail.com>
2026-08-04 17:40:20 -0300

    mais um resumao de operadores

---
+++ praticas/maisresumo-operadores/script/script.js
@@ -0,0 +1,103 @@
+//operadores de comparação
+/*
+let nome = 'Gabriel';
+
+if (nome == 'Vinicius') {
+    console.log('Seu nome é Vinicius');
+} else {
+    console.log('Seu nome não é Vinicius');
+}
+
+if (nome != 'Vinicius') {
+    console.log('Não é Vinicius');
+}
+
+let idade = 67
+
+if (idade >= 18) {
+    console.log('Você pode ir para a balada');
+}
+
+if (idade <= 17) {
+    console.log('Você é menor de idade');
+}
+*/
+
+//else e else if
+/*
+var nome = 'Matheus'
+
+if (nome == 'Gabi') {
+    console.log('Seu nome é Gabi')
+} else if (nome == 'Janine') {
+    console.log('Seu nome é Janine')
+} else {
+    console.log('Seu nome não está na lista')
+}
+
+var idade = 20
+
+if (idade >= 18) {
+    console.log('Você pode tirar carteira de motorista')   
+} else (
+    console.log('Você não pode tirar carteira de motorista')
+)
+
+var idade2 = 17
+
+if (idade2 >= 18) {
+    console.log('Você pode sair sozinho para festa')   
+} else if (idade2 < 18) {
+    console.log('Você não pode sair sozinho para festa')
+}
+*/
+
+//comparação de valores e tipos de dados
+/*
+var idade = '18'
+
+if (idade === '18') {
+    console.log('passou')
+}
+
+if (idade !== 18) {
+    console.log('passou')
+}
+*/
+
+//operador lógico and
+/*
+var idade = 17
+var nome = 'matheus'
+
+if (idade == 17 && nome == 'Vinicius') {
+    console.log('Vinicius pode jogar no time de fuebol')
+}
+*/
+
+//operador lógico or
+/*
+var idade = 17
+var nome = 'Fabio'
+
+if (idade == 17 || nome == 'Vinicius') {
+    console.log('Pode jogar no time de futebol')
+}
+
+var valor = 18
+
+if (valor > 18 && (nome == 'Vinicius' || 10 > 5)) {
+    console.log('passou')
+}
+*/
+
+//operador lógico not
+/*
+if (!true) {
+    console.log('foi')
+}
+
+if (!false) {
+    console.log('passou')
+}
+*/
