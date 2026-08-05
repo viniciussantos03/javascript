41586be6998105cc118d786eb8c054cccb2048d8
viniciussantos03 <viniciusferreirasantos982@gmail.com>
2026-08-05 19:20:30 -0300

    mais alguns resumos

---
+++ praticas/maisresumo-operadores-estruturarepeticao/script/script.js
@@ -0,0 +1,88 @@
+// operadores 
+/*
+let x = 2
+let y = 1
+
+// soma e subtração
+console.log(x += y)
+console.log(x -= y)
+
+// multiplicação
+console.log(x *= y)
+
+// divisão
+console.log(x /= y)
+
+//loop
+console.log(x++)
+console.log(x--)
+*/
+// estrutura de repetição while
+/*
+var x = 0
+
+while (x <= 10) {
+    console.log('testando ' + x)
+    x++
+}
+
+let arr = ['test', 'testando', 'a', 'b']
+var y = 0
+
+while (y <= 3) {
+    console.log(arr[y])
+    y++
+}
+
+let b = 5
+
+while (b <= 100) {
+    console.log(b)
+    b *= 2
+}
+
+let c = 100
+
+while (c >= 0) {
+    console.log(c)
+    c -= 10
+}
+
+let d = 5
+
+while (d <= 50) {
+    console.log(d)
+    d += 5
+}
+
+let nome = 'Vinicius'
+var y = 0
+
+while (y <= 7) {
+    console.log(nome[y])
+    y++
+}
+*/
+
+// estrutura de repetição for
+/*
+for(let x = 0; x <= 10; x++) {
+    console.log(x)
+}
+
+for(let y = 5; y <= 100; y *= 3) {
+    console.log(y)
+}
+
+for(let b = 100; b >= 0; b -= 10) {
+    console.log(b)
+}
+
+let arr = ['a', 'b', 'c', 'd', 'e']
+
+for(let j = 0; j < arr.length; j++) {
+    console.log(arr[j])
+}
+
+console.log(arr.length)
+*/
