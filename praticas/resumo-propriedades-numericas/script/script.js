73079464288bcff9f8773ee5c2a5294eb230ad94
viniciussantos03 <viniciusferreirasantos982@gmail.com>
2026-08-06 18:13:02 -0300

    resumo Number

---
+++ praticas/resumo-propriedades-numericas/script/script.js
@@ -0,0 +1,29 @@
+// parseFloat
+
+console.log(parseFloat(12.99))
+console.log(Number.parseFloat(12.99))
+
+// parseInt
+
+console.log(parseInt(12))
+console.log(parseInt(12.999999))
+
+// toFixed
+
+console.log(12.4942345.toFixed(3))
+console.log(12.919999999.toFixed(1))
+
+// isNaN
+
+console.log(isNaN('gambare'))
+console.log(isNaN(10))
+console.log(isNaN('3'))
+
+// MAX_VALUE e MIN_VALUE
+
+console.log(Number.MAX_VALUE)
+console.log(Number.MIN_VALUE)
+
+console.log(2.7976931348623157e+308)
+
+console.log(5e-327)
