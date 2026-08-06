57be7d2bb6d07e1d55abed650230452a84ee483b
viniciussantos03 <viniciusferreirasantos982@gmail.com>
2026-08-06 17:30:17 -0300

    resumo escopo

---
+++ praticas/resumo-escopo/script/script.js
@@ -0,0 +1,30 @@
+let x = 1
+let y = 2
+
+console.log(x, y)
+
+function teste() {
+    let z = 0
+    
+    console.log(z)
+
+    console.log(x)
+}
+
+teste()
+
+function testando() {
+    let z = 5
+
+    console.log(z)
+}
+
+testando()
+
+if(5 > 2) {
+    let v = 1
+
+    console.log(v)
+}
+
+console.log(v)
