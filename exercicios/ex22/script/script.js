50b8c839c9a2492a9a9e8fc9136a1139125e7435
viniciussantos03 <viniciusferreirasantos982@gmail.com>
2026-08-11 18:59:33 -0300

    metodos de array

---
+++ exercicios/ex22/script/script.js
@@ -0,0 +1,27 @@
+// splice
+
+const arr = [1, 2, 3, 4, 5]
+
+console.log(arr)
+
+arr.splice(2, 0, 300)
+
+console.log(arr)
+
+arr.splice(4, 1)
+
+console.log(arr)
+
+// indexOf
+
+console.log(arr.indexOf(300))
+
+// join
+
+const arr2 = ['O', 'rato', 'roeu', 'a', 'roupa']
+
+console.log(arr2.join(' '))
+
+// reverse 
+
+console.log(arr2.reverse())
