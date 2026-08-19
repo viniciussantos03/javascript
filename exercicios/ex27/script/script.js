1996d7bcec50143a8b37084c9c284c4893097ec9
viniciussantos03 <viniciusferreirasantos982@gmail.com>
2026-08-19 14:54:32 -0300

    clearTimeout e clearInterval

---
+++ exercicios/ex27/script/script.js
@@ -0,0 +1,28 @@
+// clearTimeout
+
+let x = 0
+
+let myTimer = setTimeout( function() {
+    console.log('x é igual a 0')
+}, 1500)
+
+x = 5
+
+if(x > 0) {
+
+    clearTimeout(myTimer)
+    console.log('O x passou de 0')
+}
+
+// clearInterval
+
+let myInterval = setInterval( function() {
+    console.log('Imprimindo')
+}, 500)
+
+setTimeout( function() {
+
+    console.log('Não precisamos mais repetir!')
+    clearInterval(myInterval)
+
+}, 1500)
